a8b0491884d5eed1a3fcc408dbd1d3cc4685eb06
denis <denispernach@yandex.ru>
2022-07-13 20:38:41 +0200

    if else command

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же маша");
+}
+else
+{
+    Console.Write("привет ");
+    Console.WriteLine(username);
+}
