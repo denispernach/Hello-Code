2265e40527da64b5f05bb31e3376d3d5b2e6457f
denis <denispernach@yandex.ru>
2022-07-21 17:10:05 +0200

    найти индекс числа

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 22, 35, 24, 35, 356, 77, 38};
+int n = array.Length;
+int find = 35;
+int index = 0;
+while (index < n)
+{
+if (array [index] == find)
+{
+    Console.WriteLine (index);
+    break;
+}
+index = index+1;
+}
